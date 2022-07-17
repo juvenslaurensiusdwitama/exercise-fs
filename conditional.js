57b7b05bf1e1ef3f3f1f27080ec90f2f146eb1aa
Juvens Laurensius Dwitama <juvenslaurensiusdwitama@gmail.com>
2022-07-17 11:11:47 +0700

    condition juga

--- conditional.js
+++ conditional.js
@@ -20,17 +20,34 @@
 //     console.log("Umurmu sudah terlalu tua"); 
 // }
 
-var beverages = "TEH";
-switch(beverages.toLowerCase()){
-    case 'kopi':
-        console.log('Kamu memilih kopi')
-        break;
-    case 'teh':
-        console.log('Kamu memilih teh')
-        break;
-    default:
-        console.log('Maaf, Minumanmu tidak tersedia')
-}
+// var beverages = "TEH";
+// switch(beverages.toLowerCase()){
+//     case 'kopi':
+//         console.log('Kamu memilih kopi')
+//         break;
+//     case 'teh':
+//         console.log('Kamu memilih teh')
+//         break;
+//     default:
+//         console.log('Maaf, Minumanmu tidak tersedia')
+// } 
+
+var nilai = 97;
+
+if(nilai <= 100 && nilai >= 85){
+    console.log("Grade A");
+}else if(nilai < 85 && nilai >= 70){
+    console.log("Grade B");
+}else if(nilai < 70 && nilai >= 50){
+    console.log("Grade C");
+}else if(nilai < 50 && nilai >= 30){
+    console.log("Grade D");
+}else if(nilai < 30 && nilai >= 0){
+    console.log("Grade D");
+}else{
+    console.log("Input nilai antara 0 - 100");
+} 
+
 
 
 
