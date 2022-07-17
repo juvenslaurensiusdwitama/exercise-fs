73996612fccb66ca464de68c305d8f00213fde8f
Juvens Laurensius Dwitama <juvenslaurensiusdwitama@gmail.com>
2022-07-17 10:52:11 +0700

    belajar switch case dan conditional

---
+++ conditional.js
@@ -0,0 +1,36 @@
+// conditional
+
+// console.log("Haloo");
+// var umur = 70;
+// var tinggi = 150;
+
+// if (umur >= 17 && umur < 50){
+//     if(tinggi > 160){
+//         console.log("Kamu bisa menaiki wahana ini");
+//     }else{
+//         console.log("Kamu tidak boleh menaiki wahana ini");
+//     }
+// }else if (umur < 17){
+//     if(tinggi > 160){
+//         console.log("Umur kamu belum mencukupi kriteria walaupun sudah cukup tinggi");
+//     }else{
+//         console.log("Umur dan tinggi kamu tidak memenuhi persyaratan");
+//     }
+// }else{
+//     console.log("Umurmu sudah terlalu tua"); 
+// }
+
+var beverages = "TEH";
+switch(beverages.toLowerCase()){
+    case 'kopi':
+        console.log('Kamu memilih kopi')
+        break;
+    case 'teh':
+        console.log('Kamu memilih teh')
+        break;
+    default:
+        console.log('Maaf, Minumanmu tidak tersedia')
+}
+
+
+
