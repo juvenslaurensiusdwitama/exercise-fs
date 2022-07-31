a05a50f8f7c68a8717ad14d8a6b034500e7481b6
Juvens Laurensius Dwitama <juvenslaurensiusdwitama@gmail.com>
2022-07-31 15:44:50 +0700

    new update

---
+++ belajar.js
@@ -0,0 +1,43 @@
+// LATIHAN
+/**
+ * Study Case 2
+ * 
+ * Diketahui sebuah angka fibonacci.
+ * 
+ * let fibonacci = [1,1,2,3,5,8,13,21]
+ * 
+ * Buat function :
+ * a. Function untuk mengkalikan dengan diri nya sendiri (pangkat 2)
+ * - [1,1,4,9,25,64,169,441]
+ * b. Function untuk memilih angka di atas 5
+ * - [9,25,64,169,441]
+ * c. Function untuk tampilkan angka-angka tsb
+ * - Result :
+ * 9
+ * 25
+ * 64
+ * 169
+ * 441
+ * 
+ * Buat dalam ES5 dan ES6
+ */
+
+// ES5
+
+
+// ES6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
